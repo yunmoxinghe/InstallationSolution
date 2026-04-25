c32649d612d77bafd4434ff37f209873053341e1
云漠星河 <yunmoxinghe@outlook.com>
2026-04-25 22:30:05 +0800

    ``` feat(InstallerGenerator): 更新应用版本号并修改显示名称
    - 将应用版本从 1.0.4.0 升级到 1.0.7.0
    - 更新 ARM64 和 x64 平台的安装包文件路径
    - 将应用显示名称从 "Installer Generator" 改为 "自安装包生成器"
    - 同步更新中英文资源文件中的应用标题
    - 更新程序集信息版本哈希值
    ```

--- InstallerUI/obj/ARM64/Release/net8.0-windows10.0.19041.0/win-x64/InstallerUI.AssemblyInfo.cs
+++ InstallerUI/obj/ARM64/Release/net8.0-windows10.0.19041.0/win-x64/InstallerUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstallerUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e55e740d42f88471db3564ec9222e098dfb0e81d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b57e38b86e2ed5442541df855feb06ef50e5db")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstallerUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstallerUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
