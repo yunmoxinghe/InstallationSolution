c35a1bdc47c7844ec1d1d872a5d9d10d8ae89ae3
云漠星河 <yunmoxinghe@outlook.com>
2026-05-02 21:05:16 +0800

    成功提交

--- InstallerGuard/obj/x64/Debug/net8.0-windows10.0.19041.0/InstallerGuard.AssemblyInfo.cs
+++ InstallerGuard/obj/x64/Debug/net8.0-windows10.0.19041.0/InstallerGuard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstallerGuard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2640e84c998bb970a367612406b76d743258223")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd41bf464a87aafd9c43634a4d8724d39ded2bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstallerGuard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstallerGuard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
